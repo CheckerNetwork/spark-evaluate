f632a0b57099c4177d3d21e316a7e745a31e89a8
Nikolas Haimerl <113891786+NikolasHaimerl@users.noreply.github.com>
2025-02-26 11:47:37 +0100

    Update test/public-stats.test.js
    Co-authored-by: Julian Gruber <julian@juliangruber.com>

--- test/public-stats.test.js
+++ test/public-stats.test.js
@@ -691,7 +691,7 @@ describe('public-stats', () => {
         // Separate the measurements into two groups, one for the client f0 and f1 and the other only for f1
         const findDealClients = (minerId, cid) => {
           // Check that the CID is passed correctly
-          assert.deepEqual(cid, VALID_MEASUREMENT.cid)
+          assert.strictEqual(cid, VALID_MEASUREMENT.cid)
           switch (minerId) {
             case 'f01test':
               return ['f0client', 'f1client']
