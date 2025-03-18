f96b4a0af29f31122fd1d1ee8edbdc07e0dac476
Nikolas Haimerl <nikolas@meridian.space>
2025-03-18 09:10:25 +0100

    fmt

---
+++ migrations/025.do.add-per-allocator-rsr-table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE daily_allocator_retrieval_stats (
+  day DATE NOT NULL,
+  allocator_id TEXT NOT NULL,
+  total INTEGER NOT NULL,
+  successful INTEGER NOT NULL, 
+  successful_http INTEGER NOT NULL,
+  PRIMARY KEY (day, client_id)
+);
