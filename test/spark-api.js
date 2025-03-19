1999a6b7c8caf9c068798601849007f826fc5290
Nikolas Haimerl <nikolas@meridian.space>
2025-03-19 10:53:55 +0100

    add integration test

--- test/spark-api.js
+++ test/spark-api.js
@@ -8,30 +8,30 @@ describe('spark-api client', () => {
     this.timeout(10_000)
     const { retrievalTasks, maxTasksPerNode, ...details } = await fetchRoundDetails(
       '0x8460766edc62b525fc1fa4d628fc79229dc73031',
-      12600n,
+      27834n,
       recordTelemetry
     )
 
     assert.deepStrictEqual(details, {
-      roundId: '18024', // BigInt serialized as String,
-      startEpoch: '4158303'
+      roundId: '33258', // BigInt serialized as String,
+      startEpoch: '4802338'
     })
 
     assert.strictEqual(typeof maxTasksPerNode, 'number')
 
-    assert.strictEqual(retrievalTasks.length, 1000)
+    assert.strictEqual(retrievalTasks.length, 666)
     assert.deepStrictEqual(retrievalTasks.slice(0, 2), [
       {
-        cid: 'bafkreia3oovvt7sws7wnz43zbr33lsu2yrdmx4mqswdumravjrnxfoxdka',
-        minerId: 'f02228866',
-        clients: ['f01990536'],
-        allocators: null
+        cid: 'bafk2bzaceas6yo7vfxvjcjfrkls6dtddpq5nkhgbaunlqmb6aulofuhbbq4sy',
+        minerId: 'f03499325',
+        clients: ['f03498278'],
+        allocators: ['f03358620']
       },
       {
-        cid: 'bafkreibipuscsrko7tlrw62rttqvbma3qqqkksjoi6bhvwp27qaylwupp4',
-        minerId: 'f02982293',
-        clients: ['f03064945'],
-        allocators: null
+        cid: 'bafk2bzaceaxtimy3b6dcubgj4xe2fs76gmo7h5kcraaoxjegei5mp7ddkaply',
+        minerId: 'f03322378',
+        clients: ['f03441573'],
+        allocators: ['f03019261']
       }
     ])
   })
